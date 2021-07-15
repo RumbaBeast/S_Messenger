4e22c467356d021051aff433eaa056d384197f1e
RumbaBeast <nvkornienko@gmail.com>
2021-07-15 13:01:11 +0300

    Add /Messenger

---
+++ Messenger/receiver.py
@@ -0,0 +1,26 @@
+import time
+from datetime import datetime
+
+import requests
+
+
+def print_messages(base):
+    for message in base:
+        print(datetime.fromtimestamp(message['time']), message['name'])
+        print(message['text'])
+        print()
+
+
+after = 0
+
+while True:
+    r = requests.get(
+        'http://127.0.0.1:5000/messages',
+        params={'after': after}
+    )
+    messages = r.json()['messages']
+    if messages:
+        print_messages(messages)
+        after = messages[-1]['time']
+
+    time.sleep(1)
